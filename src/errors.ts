b983655a37bf062218d050df99e14df019bd5397
Ja-boop <56199168+Ja-boop@users.noreply.github.com>
2023-05-05 17:06:56 -0300

    Throw error when a request fails

--- src/errors.ts
+++ src/errors.ts
@@ -1,3 +1,4 @@
 export class TooFewOffersError extends Error {}
 export class RequestLoanError extends Error {}
 export class WithdrawCollateralError extends Error {}
+export class RequestError extends Error {}
