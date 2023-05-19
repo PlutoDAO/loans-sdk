1668c1f037456eb1b96bbe9269670fe6bbda7553
Ja-boop <56199168+Ja-boop@users.noreply.github.com>
2023-05-19 16:48:59 -0300

    feat: update get loan (#21)

--- src/LoanAssetRequest.ts
+++ src/LoanAssetRequest.ts
@@ -1,13 +1,17 @@
 import type ILoanAssetRequest from './types/ILoanAssetRequest';
 
 export class LoanAssetRequest implements ILoanAssetRequest {
-    public code: string;
-    public issuer: string;
+    public code?: string;
+    public issuer?: string;
     public isNative: boolean;
 
-    constructor(assetCode: string, assetIssuer: string, isNative: boolean) {
+    constructor(isNative: boolean, assetCode?: string, assetIssuer?: string) {
         this.code = assetCode;
         this.issuer = assetIssuer;
         this.isNative = isNative;
+
+        if (isNative) {
+            this.code = 'XLM';
+        }
     }
 }
